cccd8aca2748ede4a722f0dc1eee0449181739a6
Sam Russell <samrussell63@gmail.com>
2017-05-11 21:17:25 -0700

    adding a reactive form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 import { ProfileComponent } from "./profile.component";
@@ -16,7 +16,8 @@ import { LoginComponent } from "./login.component";
     imports: [ 
         CommonModule,
         UserRoutingModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: []
