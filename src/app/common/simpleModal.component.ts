b1f4c4457bf5d050a4029fe9b539e868e649baba
Sam Russell <samrussell63@gmail.com>
2017-05-21 11:12:24 -0700

    adding setting for closing modal

--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject, ElementRef, ViewChild } from '@angular/core';
+import { JQ_TOKEN } from "./index";
 
 @Component({
     selector: 'simple-modal',
     template: `
-        <div id="{{elementId}}" class="modal fade" tabindex="-1">
+        <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                         <h4 class="modal-title">{{title}}</h4>
                     </div>
-                    <div class="modal.body">
+                    <div class="modal-body" (click)="closeModal()">
                         <ng-content></ng-content>
                     </div>
                 </div>
@@ -25,8 +26,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SimpleModalComponent implements OnInit {
     @Input() title: string;
     @Input() elementId: string;
+    @Input() closeOnBodyClick: string;
+    @ViewChild('modalcontainer') containerEl: ElementRef;
 
-    constructor() { }
+    constructor(@Inject(JQ_TOKEN) private $: any) { }
 
     ngOnInit() { }
+
+    closeModal() {
+        if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+            this.$(this.containerEl.nativeElement).modal('hide');
+        }
+    }
 }
