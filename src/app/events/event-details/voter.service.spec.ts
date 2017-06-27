cae4043e8733bcfe664c1fd0b4a6f27f254778bc
Sam Russell <samrussell63@gmail.com>
2017-06-26 22:59:52 -0700

    adding isolated component tests

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -7,7 +7,7 @@ describe('Voter Service', () => {
     let mockHttp;
 
     beforeEach(() => {
-        mockHttp = jasmine.createSpyObj('mockHttp', ['delete']);
+        mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
         voterService = new VoterService(mockHttp);
     });
 
@@ -33,13 +33,13 @@ describe('Voter Service', () => {
     });
 
     describe('add voter', () => {
-        it('should call http.delete wit hthe right URL', () => {
+        it('should call http.post with the right URL', () => {
             var session = { id: 6, voters: [ "john"]};
             mockHttp.post.and.returnValue(Observable.of(false));
             // force cast so you don't need all the properties
             voterService.addVoter(3, <ISession>session, "joe");
             //jasmin.any(<T>) creates an object of a type!
-            expect(mockHttp.post).toHaveBeenCalledWith('http://localhost:8808/api/events/3/sessions/6/voters/joe', "{}", jasmine.any(Object));
+            expect(mockHttp.post).toHaveBeenCalledWith('http://localhost:8808/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
         })
     })
 });
